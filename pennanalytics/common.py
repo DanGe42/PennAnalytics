e120de77eeb4d1fa9e71af30d3b489835739d456
Daniel Ge <dange@seas.upenn.edu>
2014-03-21 16:32:30 -0400

    Incorporate Bill's changes

--- pennanalytics/common.py
+++ pennanalytics/common.py
@@ -1,15 +1,20 @@
-from __future__ import print_function
+# link: bytes received/sent since last tick, bytes received/sent % bandwidth since last tick, bytes receieved/sent % bandwidth cumulative average
+# node: capacity, bytes received/sent since last tick
 
 import collections
 
+class NetworkLink(object):
 
-class NetworkStats(object):
-
-    def __init__(self, bytes_recv=0, bytes_sent=0):
+    def __init__(self, remote_sys_name, capacity, bytes_recv=0, bytes_sent=0):
+        self.remote_sys_name = remote_sys_name
+        self.capacity = capacity
         self.bytes_recv_hist = collections.deque(maxlen=10)
         self.bytes_sent_hist = collections.deque(maxlen=10)
         self.bytes_recv_hist.appendleft(bytes_recv)
         self.bytes_sent_hist.appendleft(bytes_sent)
+        self.requests = 0
+        self.input_util_avg = 0.0
+        self.output_util_avg = 0.0
 
     @property
     def bytes_recv(self):
@@ -24,36 +29,46 @@ class NetworkStats(object):
         return self.bytes_recv_hist[0] + self.bytes_sent_hist[0]
 
     def __repr__(self):
-        return "NetworkStats(bytes_recv=%d, bytes_sent=%d)" \
-               % (self.bytes_recv, self.bytes_sent)
+        return "NetworkLink(remote_sys_name=%s, capacity=%d, bytes_recv=%d, bytes_sent=%d)" % (self.remote_sys_name, self.capacity, self.stats, self.bytes_recv, self.bytes_sent)
+
+    def bytes_recv_delta(self):
+        return self.bytes_recv_hist[0] - self.bytes_recv_hist[1]
+
+    def bytes_sent_delta(self):
+        return self.bytes_sent_hist[0] - self.bytes_sent_hist[1]
+
+    # TODO: remove time interval hard codes
+    def input_utilization(self):
+        return float(self.bytes_recv_delta()*8)/(8*self.capacity)
+
+    def output_utilization(self):
+        return float(self.bytes_sent_delta()*8)/(8*self.capacity)
 
     def update(self, bytes_recv, bytes_sent):
+        self.requests += 1
         self.bytes_recv_hist.appendleft(bytes_recv)
         self.bytes_sent_hist.appendleft(bytes_sent)
 
-    def serialize(self):
-        return {
-            "bytes_recv_history": list(self.bytes_recv_hist),
-            "bytes_sent_history": list(self.bytes_sent_hist)
-        }
-
-
-class NetworkLink(object):
-
-    def __init__(self, remote_sys_name, capacity, stats):
-        self.remote_sys_name = remote_sys_name
-        self.capacity = capacity
-        self.stats = stats
-
-    def __repr__(self):
-        return "NetworkLink(remote_sys_name=%s, capacity=%d, stats=%s)" \
-               % (self.remote_sys_name, self.capacity, self.stats)
+        if self.requests >= 2:
+            self.input_util_avg = self.input_util_avg + (self.input_utilization() - self.input_util_avg)/(self.requests-1)
+            self.output_util_avg = self.output_util_avg + (self.output_utilization() - self.output_util_avg)/(self.requests-1)
 
     def serialize(self):
         return {
             "remote_sys_name": self.remote_sys_name,
             "capacity": self.capacity,
-            "stats": self.stats.serialize()
+            "stats": {
+                "bytes_recv_hist": list(self.bytes_recv_hist),
+                "bytes_sent_hist": list(self.bytes_sent_hist),
+                "total_bytes_recv": self.bytes_recv,
+                "total_bytes_sent": self.bytes_sent,
+                "bytes_recv_last_interval": self.bytes_recv_delta(),
+                "bytes_sent_last_interval": self.bytes_sent_delta(),
+                "bytes_recv_moving_average": self.input_util_avg,
+                "bytes_sent_moving_average": self.output_util_avg,
+                "upload_utilization": self.output_utilization(),
+                "download_utilization": self.input_utilization()
+            }
         }
 
 
@@ -68,26 +83,28 @@ class NetworkNode(dict):
         return self._sys_name
 
     def __repr__(self):
-        return "NetworkNode('%s', %s)" \
-               % (self.sys_name, super(NetworkNode, self).__str__())
+        return "NetworkNode('%s', %s)" % (self.sys_name, super(NetworkNode, self).__str__())
 
-    def add_remote(self, port, remote_sys_name, capacity, stats=NetworkStats()):
-        self[port] = NetworkLink(remote_sys_name, capacity, stats)
+    def add_remote(self, port, remote_sys_name, capacity):
+        self[port] = NetworkLink(remote_sys_name, capacity, 0, 0)
 
     def update_remote(self, port, bytes_recv, bytes_sent):
-        self[port].stats.update(bytes_recv, bytes_sent)
+        self[port].update(bytes_recv, bytes_sent)
 
-    def capacity(self):
+    def total_capacity(self):
         return sum(link.capacity for link in self.itervalues())
 
     def total_bytes_received(self):
-        return sum(link.stats.bytes_recv for link in self.itervalues())
+        return sum(link.bytes_recv for link in self.itervalues())
 
     def total_bytes_sent(self):
-        return sum(link.stats.bytes_sent for link in self.itervalues())
+        return sum(link.bytes_sent for link in self.itervalues())
 
-    def total_bytes_transferred(self):
-        return sum(link.stats.bytes_transferred for link in self.itervalues())
+    def total_bytes_received_delta(self):
+        return sum(link.bytes_recv_delta() for link in self.itervalues())
+
+    def total_bytes_sent_delta(self):
+        return sum(link.bytes_sent_delta() for link in self.itervalues())
 
     def serialize(self):
         return {
@@ -98,6 +115,13 @@ class NetworkNode(dict):
 
 if __name__ == "__main__":
     n = NetworkNode("hub")
+
     n.add_remote(1, "sys1", 100)
-    for l in n:
-        print(n[l])
+    n.update_remote(1, 10, 10)
+    n.update_remote(1, 20, 20)
+    n.update_remote(1, 40, 40)
+    n.update_remote(1, 70, 70)
+
+    print n[1].requests
+    print n[1].input_util_avg
+    print n[1].output_util_avg
