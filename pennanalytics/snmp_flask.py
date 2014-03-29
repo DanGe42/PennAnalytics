a31e83f60bf28e2ef42d7799b90de3a08394ee54
Daniel Ge <dange@seas.upenn.edu>
2014-03-29 03:16:35 -0400

    Fix snmp directory

--- pennanalytics/snmp_flask.py
+++ pennanalytics/snmp_flask.py
@@ -6,7 +6,8 @@ import config
 import snmp_fetch
 
 app = Flask(__name__)
-MIB_DIRECTORY = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'snmp')
+MIB_DIRECTORY = os.path.join(os.path.dirname(
+    os.path.realpath(__file__)), '..', 'snmp')
 
 @app.route("/all")
 def fetch_all_data():
